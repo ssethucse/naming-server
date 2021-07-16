521f928ce51258c97c5bdc8dee249d2781afa630
sethu <ssethucse@gmail.com>
2021-07-16 15:18:48 +0530

    Initial commit

---
+++ src/test/java/com/pos/config/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.config.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
